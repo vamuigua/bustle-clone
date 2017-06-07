7c8b7a07a6fc8eda9bea9e138f76887c3a211ddc
Victor Allen <vamuigua@gmail.com>
2017-06-07 17:05:37 +0300

    Modified to smaller images

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,17 +4,17 @@ var contents= [{
   id: 1,
   headlines: "Uhuru Lauches Delivery.go.ke",
   author: "Victor Allen",
-  image: "http://presidential-power.com/wp-content/uploads/2017/01/Kenya-Uhuru-Kenyatta.jpg"
+  image: "http://www.kenyadigest.com/wp-content/uploads/2015/07/index5.jpg"
 }, {
   id: 2,
   headlines: "Real Madrid Wins",
   author: "CR7",
-  image: "https://img.rt.com/files/2016.05/original/574a17e8c46188e7558b45d0.jpg"
+  image: "http://stella9ja.com/wp-content/uploads/2016/05/CjkyBKzXAAEDr5m.jpgsmall.jpg"
 }, {
   id: 3,
   headlines: "Latest Start Ups",
   author: "Tony",
-  image: "http://www.thecareermuse.co.in/wp-content/uploads/2016/09/Startups.jpg"
+  image: "http://www.transfirst.com/images/uploads/business-incubators-the-secret-weapon-for-startups-and-small-businesses.jpg"
 }];
 
 export default Ember.Route.extend({
